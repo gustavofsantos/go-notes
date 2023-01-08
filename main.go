ea48610e111e553be77af93d5882917706339009
Gustavo Santos <gustavofsantos@outlook.com>
2023-01-08 11:14:36 -0300

    format main file

--- main.go
+++ main.go
@@ -11,68 +11,68 @@ import (
 )
 
 type Config struct {
-  path string
+	path string
 }
 
 type Notification struct {
-  text string
+	text string
 }
 
 // read this from a config file in the future
 func GetConfig() Config {
-  return Config{path: "/home/gustavo/notes/"}
+	return Config{path: "/home/gustavo/notes/"}
 }
 
 func GetTodayDateAsString() string {
-  layout := "2006-01-02"
-  now := time.Now()
-  return fmt.Sprint(now.Format(layout))
+	layout := "2006-01-02"
+	now := time.Now()
+	return fmt.Sprint(now.Format(layout))
 }
 
 func AppendMarkdownExtension(date string) string {
-  return date + ".md"
+	return date + ".md"
 }
 
 func GetFilePath(notesPath string, filename string) string {
-  return notesPath + "journal/" + filename
+	return notesPath + "journal/" + filename
 }
 
 func NotifyGnome(notification Notification) {
-  err := exec.Command("notify-send", "-u", "critical", "Atenção: Reunião", fmt.Sprintf("%s", notification.text)).Run()
-  if err != nil {
-    panic(err)
-  }
+	err := exec.Command("notify-send", "-u", "critical", "Atenção: Reunião", fmt.Sprintf("%s", notification.text)).Run()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func NotifyTmux(notification Notification) {
-  err := exec.Command("tmux", "display-message", "-d", "5000", fmt.Sprintf("%s", notification.text)).Run()
-  if err != nil {
-    log.Println("Error displaying using tmux. Tmux is running?")
-  }
+	err := exec.Command("tmux", "display-message", "-d", "5000", fmt.Sprintf("%s", notification.text)).Run()
+	if err != nil {
+		log.Println("Error displaying using tmux. Tmux is running?")
+	}
 }
 
 func main() {
-  config := GetConfig()
-  filename := AppendMarkdownExtension(GetTodayDateAsString())
-  filepath := GetFilePath(config.path, filename)
-  filedata, err := os.ReadFile(filepath)
-  if err != nil {
-    panic(err)
-  }
-  journal := string(filedata)
-  meetings := core.ParseMeetings(journal)
-  now := time.Now()
-  hour := fmt.Sprintf("%02d", now.Local().Hour())
-  minutes := fmt.Sprintf("%02d", now.Local().Minute())
+	config := GetConfig()
+	filename := AppendMarkdownExtension(GetTodayDateAsString())
+	filepath := GetFilePath(config.path, filename)
+	filedata, err := os.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+	journal := string(filedata)
+	meetings := core.ParseMeetings(journal)
+	now := time.Now()
+	hour := fmt.Sprintf("%02d", now.Local().Hour())
+	minutes := fmt.Sprintf("%02d", now.Local().Minute())
 
-  for _, meeting := range meetings {
-    meetingHour := core.GetHour(meeting)
-    meetingMinutes := core.GetMinutes(meeting)
-    text := core.GetText(meeting) 
-    if hour == meetingHour && minutes == meetingMinutes {
-      notification := Notification{ text: text }
-      NotifyGnome(notification)
-      NotifyTmux(notification)
-    }
-  }
+	for _, meeting := range meetings {
+		meetingHour := core.GetHour(meeting)
+		meetingMinutes := core.GetMinutes(meeting)
+		text := core.GetText(meeting)
+		if hour == meetingHour && minutes == meetingMinutes {
+			notification := Notification{text: text}
+			NotifyGnome(notification)
+			NotifyTmux(notification)
+		}
+	}
 }
